349133df0b6ce1a462832f3ec8f8865f5a0dc9b9
Olgadz469 <133228372+Olgadz469@users.noreply.github.com>
2023-11-28 17:56:51 +0200

    3

---
+++ src/Servises/getMovies.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '62144ae074e6d6ae76b1ff5ee4b62990';
+
+export const fetchTrending = async () => {
+  const response = await axios.get(
+    `${API_URL}trending/movie/day?api_key=${API_KEY}&language=en-US`
+  );
+
+  return response.data.results;
+};
+
+export const fetchSearchByKeyword = async keyword => {
+  const response = await axios.get(
+    `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${keyword}`
+  );
+  return response.data.results;
+};
+
+export const fetchMovieDetails = async movieId => {
+  const response = await axios.get(
+    `${API_URL}search/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+  );
+  return response.data;
+};
+
+export const fetchActors = async movieId => {
+  const response = await axios.get(
+    `${API_URL}search/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
+  );
+  return response.data.cast;
+};
+
+export const fetchReviews = async movieId => {
+  const response = await axios.get(
+    `${API_URL}search/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return response.data.results;
+};
